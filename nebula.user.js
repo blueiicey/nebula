1e7955d8923864e8e8f698a85fe02949a3f04334
ice <79723907+blueiicey@users.noreply.github.com>
2024-10-05 17:37:25 -0400

    Create nebula.user.js

---
+++ nebula.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        Nebula Predictor
+// @namespace   nebula.negro.lol
+// @version     1.0
+// @author      Nebula Services
+// @description #1 predictor on the market
+// @icon        https://raw.githubusercontent.com/blueiicey/nebula/main/logo.png
+// @match       https://bloxflip.com/*
+// @grant       GM_addElement
+// @grant       GM_addStyle
+// @grant       GM_xmlhttpRequest
+// @grant       unsafeWindow
+// @connect     *
+// @run-at      document-end
+// @downloadURL https://github.com/blueiicey/nebula/raw/main/nebula.user.js
+// @updateURL   https://github.com/blueiicey/nebula/raw/main/nebula.user.js
+// ==/UserScript==
