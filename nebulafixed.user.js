976ec127f0220604e147c4e24970369264e6b87c
icey <79723907+blueiicey@users.noreply.github.com>
2024-11-28 13:14:48 -0500

    nebula fixed
    its 1 line come on.

---
+++ nebulafixed.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         Nebula Predictor
+// @namespace    https://nebula.negro.lol/
+// @icon         https://nebula.negro.lol/logo.png
+// @version      ESP
+// @description  The BEST ESP on the market!
+// @author       JP, Icey, darian, PwkDev
+// @match        https://bflip.com/*
+// @grant        GM_addStyle
+// @grant        GM_xmlhttpRequest
+// @updateURL    https://nebula.negro.lol/nebula.user.js
+// @downloadURL  https://nebula.negro.lol/nebula.user.js
+// @require      https://nebula.negro.lol/client.js
+// ==/UserScript==
+
+//1 line fix :O
+//icey for hire pls hire me ill make predictor
+
+
+/*
+            _           _
+           | |         | |
+ _ __   ___| |__  _   _| | __ _    ___  ___ _ __
+| '_ \ / _ \ '_ \| | | | |/ _` |  / _ \/ __| '_ \
+| | | |  __/ |_) | |_| | | (_| | |  __/\__ \ |_) |
+|_| |_|\___|_.__/ \__,_|_|\__,_|  \___||___/ .__/
+                                           | |
+                                           |_|
+*/
